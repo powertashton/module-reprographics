7231496174ee80fa77ce4661293a4e6865fff7c1
Ashton Power <48048360+powertashton@users.noreply.github.com>
2021-02-22 14:49:56 +0800

    gateways

---
+++ Reprographics/src/Domain/ItemGateway.php
@@ -0,0 +1,31 @@
+<?php
+namespace Gibbon\Module\Reprographics\Domain; //Replace ModuleName with your module's name, ommiting spaces
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * Name Gateway
+ *
+ * @version v21
+ * @since   v21
+ */
+class ItemGateway extends QueryableGateway //Replace NameGateway with the name of the gateway, 
+{
+    use TableAware;
+
+    private static $tableName = 'Item'; //The name of the table you will primarily be querying
+    private static $primaryKey = 'itemID'; //The primaryKey of said table
+    private static $searchableColumns = []; // Optional: Array of Columns to be searched when using the search filter
+    
+    public function selectItems() {
+        $select = $this
+            ->newSelect()
+            ->from('Item')
+            ->cols(['itemID','subCategoryID', 'name'])
+            ->orderBy(['itemID']);
+
+        return $this->runSelect($select);
+    }
+}
---
+++ Reprographics/src/Domain/SubCategoryGateway.php
@@ -0,0 +1,31 @@
+<?php
+namespace Gibbon\Module\Reprographics\Domain; //Replace ModuleName with your module's name, ommiting spaces
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * Name Gateway
+ *
+ * @version v21
+ * @since   v21
+ */
+class SubCategoryGateway extends QueryableGateway //Replace NameGateway with the name of the gateway, 
+{
+    use TableAware;
+
+    private static $tableName = 'ItemSubCategory'; //The name of the table you will primarily be querying
+    private static $primaryKey = 'subCategoryID'; //The primaryKey of said table
+    private static $searchableColumns = []; // Optional: Array of Columns to be searched when using the search filter
+    
+    public function selectSubCategories() {
+        $select = $this
+            ->newSelect()
+            ->from('ItemSubCategory')
+            ->cols(['subCategoryID', 'categoryID' 'name'])
+            ->orderBy(['subCategoryID']);
+
+        return $this->runSelect($select);
+    }
+}
