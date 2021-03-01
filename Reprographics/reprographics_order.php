a8e74f1be63695c436c3642fb163c7e4fa0d36e1
Ashton Power <48048360+powertashton@users.noreply.github.com>
2021-03-01 15:14:05 +0800

    Update reprographics_order.php

--- Reprographics/reprographics_order.php
+++ Reprographics/reprographics_order.php
@@ -21,7 +21,8 @@ use Gibbon\Forms\Form;
 use Gibbon\Module\Reprographics\Domain\ItemGateway;
 use Gibbon\Tables\DataTable;
 use Gibbon\Module\Reprographics\Domain\OrderGateway;
-
+use Gibbon\Module\Reprographics\Domain\StaffGateway;
+use Gibbon\Module\Reprographics\Domain\DepartmentGateway;
 // Module includes
 require_once __DIR__ . '/moduleFunctions.php';
 
@@ -31,57 +32,83 @@ if (!isActionAccessible($guid, $connection2, '/modules/Reprographics/reprographi
 	// Access denied
 	$page->addError(__('You do not have access to this action.'));
 } else {
-
+    
     $moduleName = $gibbon->session->get('module');
     $gibbonPersonID = $gibbon->session->get('gibbonPersonID');
-    $form = Form::create('orderItems', $gibbon->session->get('absoluteURL') . '/modules/' . $moduleName . '/reprographics_orderProcess.php', 'post');
-    $form->addHiddenValue('address', $gibbon->session->get('address'));
     
-    $form->setClass('noIntBorder overflow-visible fullWidth standardForm');
+    $staffGateway = $container->get(StaffGateway::class);
+    $departments = $staffGateway->selectBy(['gibbonPersonID' => $gibbonPersonID])->fetchAll();
     
-    $itemGateway = $container->get(ItemGateway::class);
-    $itemData = $itemGateway->selectItems()->toDataSet()->toArray();
+    if (empty($departments)) {
+        $page->addError(__('You are not in any Reprographic Departments'));
+    } else {
+        
+        $form = Form::create('orderItems', $gibbon->session->get('absoluteURL') . '/modules/' . $moduleName . '/reprographics_orderProcess.php', 'post');
+        $form->addHiddenValue('address', $gibbon->session->get('address'));
+    
+        $form->setClass('noIntBorder overflow-visible fullWidth standardForm');
+    
+        $itemGateway = $container->get(ItemGateway::class);
+        $itemData = $itemGateway->selectItems()->toDataSet()->toArray();
 
-    $options = array_reduce($itemData, function ($group, $item) {
-      $group[$item['categoryName']][$item['subCategoryName']][$item['itemID']] = $item['itemName'];
-      return $group;
-    }, []);
+        $options = array_reduce($itemData, function ($group, $item) {
+          $group[$item['categoryName']][$item['subCategoryName']][$item['itemID']] = $item['itemName'];
+          return $group;
+        }, []);
     
-    $row = $form->addRow();
-        $row->addLabel('itemID', __('Item'));
-        $row->addDropdown('itemID')
-            ->placeholder('Please Select...')
-            ->fromArray($options)
-            ->isRequired();
+        $row = $form->addRow();
+            $row->addLabel('itemID', __('Item'));
+            $row->addDropdown('itemID')
+                ->placeholder('Please Select...')
+                ->fromArray($options)
+                ->isRequired();
+        
+        foreach ($departments as $department) {
+            $departmentGateway = $container->get(DepartmentGateway::class);
+            $departmentOptions[] = $departmentGateway->getByID($department['deptID']);
+        }
+        $departmentOptions = array_reduce($departmentOptions, function ($group, $item) {
+              $group[$item['deptID']] = $item['deptName'];
+              return $group;
+            }, []);
+        
+        $row = $form->addRow();
+            $row->addLabel('deptID', __('Department'));
+            $row->addSelect('deptID')
+                ->placeholder('Please Select...')
+                ->fromArray($departmentOptions)
+                ->isRequired();
+        
+        $row = $form->addRow();
+            $row->addLabel('quantity', __('Quantity'));
+            $row->addNumber('quantity')->setValue(1)->spinner(true)->isRequired();
     
-    $row = $form->addRow();
-        $row->addLabel('quantity', __('Quantity'));
-        $row->addNumber('quantity')->setValue(1)->spinner(true)->isRequired();
     
-    
-    $row = $form->addRow();
-        $row->addFooter();
-        $row->addSubmit();
+        $row = $form->addRow();
+            $row->addFooter();
+            $row->addSubmit();
 
-    echo $form->getOutput();
-    //SELECT BY USER
-    $orderGateway = $container->get(OrderGateway::class);
-    
-    $criteria = $orderGateway->newQueryCriteria(true)
-        ->sortBy('orderStatus', 'ASC')
-        ->sortBy('orderID', 'DESC')
-        ->fromPOST();
-    $orders = $orderGateway->queryOrders($criteria, $gibbonPersonID);
+        echo $form->getOutput();
 
-    $table = DataTable::create('orders');
-        $table->setTitle('Your Orders');
+
+        $orderGateway = $container->get(OrderGateway::class);
+    
+        $criteria = $orderGateway->newQueryCriteria(true)
+            ->sortBy('orderStatus', 'ASC')
+            ->sortBy('orderID', 'DESC')
+            ->fromPOST();
+        $orders = $orderGateway->queryOrders($criteria, $gibbonPersonID);
+
+        $table = DataTable::create('orders');
+            $table->setTitle('Your Orders');
 
         
-        $table->addColumn('orderID', __('orderID'));
-        $table->addColumn('gibbonPersonID', __('gibbonPersonID'));
-        $table->addColumn('itemID', __('itemID'));
-        $table->addColumn('quantity', __('quantity'));
-        $table->addColumn('orderStatus', __('orderStatus'));
-        $table->addColumn('orderDate', __('orderDate'));
-    echo $table->render($orders);
+            $table->addColumn('orderID', __('orderID'));
+            $table->addColumn('gibbonPersonID', __('gibbonPersonID'));
+            $table->addColumn('itemID', __('itemID'));
+            $table->addColumn('quantity', __('quantity'));
+            $table->addColumn('orderStatus', __('orderStatus'));
+            $table->addColumn('orderDate', __('orderDate'));
+        echo $table->render($orders);
+    }
 }	
